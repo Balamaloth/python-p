f5dc048c55340a7288615a0ceb2c2352a8236ee2
21121M4 <balasainik3@gmail.com>
2026-01-10 12:55:33 +0530

    initial commit

---
+++ data type/coomplx.py
@@ -0,0 +1,60 @@
+x = 1j
+print(x,type(x)) # compllex number 
+
+#--- IGNORE ---
+
+l = ["bala", "krishna" , 'maloth']
+print(l,type(l)) # list 
+
+#--- IGNORE ---
+
+t = ("bala", "krishna", "maloth")
+print(t,type(t)) #tuple 
+
+#--- IGNORE ---
+
+r = range(10)
+print(r , type(r))
+print(list(r)) # converting range to list
+
+#--- IGNORE ---
+
+d = {'name': 'bala', 'age':21}
+print(d,type(d)) # dict
+
+#--- IGNORE ---
+
+s = {'bala', 'krishna', 'maloth'}
+print(s,type(s)) #set
+
+#--- IGNORE ---
+
+f = frozenset({'bala', 'krishna', 'maloth'})
+print(f,type(f)) # frozenset
+
+#--- IGNORE ---
+
+b = True
+print(b,type(b)) # bool
+
+#--- IGNORE ---
+
+by = b'krishna'
+print(by,(type(by))) # bytes
+
+# --- IGNORE ---
+
+bya = bytearray(4)
+print(bya,type(bya)) # bytearray
+
+#--- IGNORE ---
+
+m = memoryview(bytes(4))
+print(m,type(m)) # memoryview
+
+#--- IGNORE ---
+
+n =  None
+print(n,type(n)) # None type
+
+#--- IGNORE ---
---
+++ numbers/num.py
@@ -0,0 +1,70 @@
+'''
+There are three numeric types in Python:
+
+int
+float
+complex
+
+
+'''
+x = 1 # int
+y = 4.4 # float
+z = 1j  # complex
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+
+m = -3432
+print(m,type(m))
+
+n = 1.1e0
+j = 1.E0 
+q = -35.5e9
+print(n,type(n))
+print(j,type(j))
+print(q,type(q))
+
+# complex numbers
+
+e = 3+5j
+f = -5j
+g = 4j
+
+print(e,type(e))
+print(f,type(f))
+print(g,type(g))
+
+ # --- IGNORE ---
+
+
+
+
+'''
+Type Conversion
+You can convert from one type to another with the int(), float(), and complex() methods:
+
+Example
+Convert from one type to another:
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+'''
+
